2831cc200c97c1462ac5988dddafa1af9a10a856
monikahthediviner <143567612+monikahthediviner@users.noreply.github.com>
2023-11-12 17:29:06 -0600

    Update chapter1.py

--- chapter1.py
+++ chapter1.py
@@ -1,2 +1,15 @@
-print("Welcome to Codeland")
-print("you stand at the enterane, ready for your coding quest")
+html_content = """
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Pythia's Zorklandish</title>
+</head>
+<body>
+  <div id="intro">
+    <h1>Welome to Pythia's Zorklandish!</h1>
+    <p>You stand at the entrance, ready for your quest.</p>
+  </div>
+</body>
+</html>
+"""
+print(html_content)
