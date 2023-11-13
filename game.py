a893ea41f0f2d0b04427d86458384a5031190ebd
monikahthediviner <143567612+monikahthediviner@users.noreply.github.com>
2023-11-12 18:51:52 -0600

    Update game.py

--- game.py
+++ game.py
@@ -1,3 +1,12 @@
 print("The Fool's Journey")
 def start_room():
   print("you find yourself in the clearing of the thicket bathed in starlight.")
+
+def next_room():
+  print("you walk through a small space between two trees.") 
+
+def second_room():
+  print("there is an altar in the middle of the small clearing"
+
+def boss_room():
+  print("a fierce boss appears!)
